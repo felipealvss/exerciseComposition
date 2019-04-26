8e602a15d83754f252939cc191d23c6147295795
Felipe Alves <felipe.silva01@aluno.unifametro.edu.br>
2019-04-26 17:15:56 -0300

    Ajustes de erros, finalização do exercício

--- src/aplication/Program.java
+++ src/aplication/Program.java
@@ -4,7 +4,6 @@ import java.text.ParseException;
 import java.text.SimpleDateFormat;
 import java.util.Date;
 import java.util.Scanner;
-
 import entities.Department;
 import entities.HourContract;
 import entities.Worker;
@@ -17,10 +16,12 @@ public class Program {
 		SimpleDateFormat sdf = new SimpleDateFormat("dd/MM/yyyy");
 		
 		// Department
+		System.out.println();
 		System.out.print("Enter department's name: ");
 			String NameDepartment = read.nextLine();
 		// Worker
-		System.out.print("Enter work data: ");
+		System.out.println();
+		System.out.println("Enter work data: ");
 		System.out.print("Name: ");
 			String nameWorker = read.nextLine();
 		System.out.print("Level: ");
@@ -34,8 +35,10 @@ public class Program {
 		// Contracts
 		System.out.print("How many contracts to this worker?: ");
 			int numberContracts = read.nextInt();
+			
 				for (int i = 0; i < numberContracts; i++) {
-					System.out.printf("Enter contract #%n data: ", i + 1);
+					System.out.println();
+					System.out.printf("Enter contract #%d data: %n", i + 1);
 					System.out.print("Date (DD/MM/YYYY): ");
 						Date dateContract = sdf.parse(read.next());
 					System.out.print("Value per hour: ");
@@ -44,22 +47,29 @@ public class Program {
 						int hourDuration = read.nextInt();
 						
 					// Contract's Instance
-					HourContract contract = new HourContract(dateContract, vph, hourDuration);
+					HourContract newContract = new HourContract(dateContract, vph, hourDuration);
 					// Definition of a NEW Contract to the Worker
-					worker.addContract(contract);
+					worker.addContract(newContract);
 				}
 		
-		// Show the informations
 		System.out.println();
+		System.out.println("+++++++++++++++++++++++++++++++++++++++++++");
+		System.out.println();
+		
+		// Show the informations
 		System.out.print("Enter month and year to calculate income (MM/YYYY): ");
 			String monthAndYear = read.next(); // Now, we will divide to receive separately two attributes
 				int month = Integer.parseInt(monthAndYear.substring(0, 2)); // Here, we assign a value to the attribute 'month'
 				int year = Integer.parseInt(monthAndYear.substring(3)); // And here, we assign a value to the attribute 'year'
 		System.out.println();
-		System.out.print("Name: " + worker.getName());
-		System.out.print("Department: " + worker.getDepartment().getName());
-		System.out.print("Income for " + monthAndYear + ": " + String.format("%.2f", worker.income(year, month)));
-			
+		System.out.println("Name: " + worker.getName());
+		System.out.println("Department: " + worker.getDepartment().getName());
+		System.out.println("Income for " + monthAndYear + ": " + String.format("%.2f", worker.income(year, month)));
+		
+		System.out.println();
+		System.out.println("+++++++++++++++++++++++++++++++++++++++++++");
+		System.out.println();
+		
 		read.close();
 	}
 
